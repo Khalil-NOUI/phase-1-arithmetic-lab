e1b106761a89f6b7960686db02c8cbab64bd903a
Khalil-NOUI <133600682+Khalil-NoOUi@users.noreply.github.com>
2023-07-23 19:06:15 +0100

    Completed assignment'

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply;
+multiply = num1 * num2;
+
+
+let random; 
+random = `${Math.random()*100}`;
+random = parseInt(random);
+
+
+let mod;
+let num3 = 9;
+let num4 = 5;
+
+mod =num3 % num4;
+
+
+let max;
+max = Math.max(14,2,3,20,18,19,10,3)
